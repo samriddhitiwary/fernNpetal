68676b4dad7df607bc05f257940eb1a8e8340829
Samriddhi Tiwary <samriddhi.tiwary01@gmail.com>
2024-09-07 18:57:10 +0530

    Created New Components Using AWS

--- src/Components/CuratedGifts/CuratedGifts.js
+++ src/Components/CuratedGifts/CuratedGifts.js
@@ -1,107 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
 import './CuratedGifts.css';
 import Card from 'react-bootstrap/Card';
+// https://fernnpatel.s3.ap-south-1.amazonaws.com/assets/CuratedGifts_assets.json
 
 const CuratedGifts = () => {
-    return (
-        <Container className='CuratedGifts_div' >
-            <div className='CuratedGifts_Heading_div' >
-                <p className='CuratedGifts_Heading'>Thoughtfully Curated Gifts    <Button variant="success" className='CuratedGifts_btn'>VIEW ALL</Button>{' '}</p>
 
+    const [curatedGiftsAssets, setCuratedGiftsAssets] = useState([]);
+    const awsUrl = "https://fernnpatel.s3.ap-south-1.amazonaws.com";
+    const assetUrl = awsUrl + "/assets/CuratedGifts_assets.json";
+
+    useEffect(() => {
+        fetch(assetUrl)
+            .then(response => response.json())
+            .then(jsonData => {
+                jsonData.map((asset) => {
+                    asset.img = awsUrl + '/' + asset.img;
+                    return asset; 
+                });
+                setCuratedGiftsAssets(jsonData);
+            });
+    }, []);
+
+    return (
+        <Container className='CuratedGifts_div'>
+            <div className='CuratedGifts_Heading_div'>
+                <p className='CuratedGifts_Heading'>
+                    Thoughtfully Curated Gifts 
+                    <Button variant="success" className='CuratedGifts_btn'>
+                        VIEW ALL
+                    </Button>
+                </p>
             </div>
-            <br></br>
-            <br></br>
+            <br /><br />
 
             <div className="CuratedGifts_image_grid">
                 <div className='CuratedGifts_img_div'>
-                    <img src='images/CuratedGifts/img1.webp' className='CuratedGifts_img_item'></img>
+                    <img src='images/CuratedGifts/img1.webp' className='CuratedGifts_img_item' alt="Gift 1" />
                 </div>
 
                 <div className='CuratedGifts_img_div'>
-                    <img src='images/CuratedGifts/img2.webp' className='CuratedGifts_img_item'></img>
+                    <img src='images/CuratedGifts/img2.webp' className='CuratedGifts_img_item' alt="Gift 2" />
                 </div>
             </div>
 
-            <br></br>
+            <br />
 
             <div className='CuratedGifts_grid_2'>
-                <div className='CuratedGifts_grid_img_2' >
-                    <Card style={{ width: '15rem',marginLeft:'2%',marginBottom:'2%'} }>
-                        <Card.Img variant="top" src="images/CuratedGifts/img3.webp" />
-                        <Card.Body >
-                            
-                            <Card.Text>
-                                 
-                              Unleash their inner fashionista with Fashion
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
-                
-
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={{ width: '15rem',marginLeft:'2%',marginBottom:'2%' }}>
-                        <Card.Img variant="top" src="images/CuratedGifts/img4.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                               Double the surprise with paired Gift Combos
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
-
-
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={{ width: '15rem',marginLeft:'2%',marginBottom:'2%' }}>
-                        <Card.Img variant="top" src="images/CuratedGifts/img5.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                                Create magical moments with Experiential Gifts
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
-
-
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={{ width: '15rem',marginLeft:'2%',marginBottom:'2%' }}>
-                        <Card.Img variant="top" src="images/CuratedGifts/img6.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                                Never fall short of surprise with our big Gift Range
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
-
-
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={{ width: '15rem',marginLeft:'2%',marginBottom:'2%' }}>
-                        <Card.Img variant="top" src="images/CuratedGifts/img7.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                                Let them relish each moment with a Cake from you
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
-
+                {curatedGiftsAssets.map((e, index) => (
+                    <div className='CuratedGifts_grid_img_2' key={index}>
+                        <Card style={{ width: '15rem' }}>
+                            <Card.Img variant="top" src={e.img} alt={e.desc} />
+                            <Card.Body>
+                                <Card.Text>{e.desc}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                ))}
             </div>
-            <br></br>
-            <br></br>
+            <br /><br />
         </Container>
-
-    )
+    );
 }
 
-export default CuratedGifts
+export default CuratedGifts;
--- src/Components/Gift_In_Trend/Gift_In_Trend.js
+++ src/Components/Gift_In_Trend/Gift_In_Trend.js
@@ -1,114 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import './Gift_In_Trend.css'
+// https://fernnpatel.s3.ap-south-1.amazonaws.com/assets/Gift_In_TrendAssets.json
 
 const Gift_In_Trend = () => {
-  return (
-    <>
-    <br></br><br></br>
-    <div>
-       
-        <Container className='Gift_In_Trend_Container'>
-            <br></br>
-        <div className='CuratedGifts_Heading_div' >
-            <p className='Gift_In_Trend_Heading'>Gifts In Trend</p>
 
-        </div>
-            <br></br>
-            
-            <div className='CuratedGifts_grid_2'>
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={ {width: '15rem',marginLeft:'2%',marginBottom:'1%'}}>
-                        <Card.Img variant="top" src="images/Gift_In_Trend/img1.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                               Personalised Anniversary Chocolate Photocake
-                               
-                            </Card.Text>
-                            <Card.Text>
-                            <b>₹999</b> 
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
+    const [Gift_In_Trend_Assets, Gift_In_Trend_setEventAssets] = useState([]);
+    const awsUrl = "https://fernnpatel.s3.ap-south-1.amazonaws.com";
+    const assetUrl = awsUrl + "/assets/Gift_In_TrendAssets.json";
 
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={ {width: '15rem',marginLeft:'2%',marginBottom:'2%'}}>
-                        <Card.Img variant="top" src="images/Gift_In_Trend/img2.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                            Chocolate Truffle Cream Cake Half kg
-                            </Card.Text>
-                            <Card.Text>
-                            <b>₹599</b> 
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
+    useEffect(() => {
 
+        fetch(assetUrl)
+            .then(response => response.json())
+            .then(jsonData => {
+                jsonData.map((asset) => {
+                    asset.img = awsUrl + '/' + asset.img;
+                    return asset; // Add return here to avoid issues with .map
+                });
+                Gift_In_Trend_setEventAssets(jsonData)
+            });
+    }, []);
 
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={ {width: '15rem',marginLeft:'2%',marginBottom:'2%'}}>
-                        <Card.Img variant="top" src="images/Gift_In_Trend/img3.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                                Mystical Unicorn Personalised Night Lamp
-                            </Card.Text>
-                            <Card.Text>
-                            <b>₹599</b> 
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
+    return (
+        <>
+            <br></br><br></br>
+            <div>
 
+                <Container className='Gift_In_Trend_Container'>
+                    <br></br>
+                    <div className='CuratedGifts_Heading_div' >
+                        <p className='Gift_In_Trend_Heading'>Gifts In Trend</p>
+                    </div>
+                    <br></br>
 
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={ {width: '15rem',marginLeft:'2%',marginBottom:'2%'}}>
-                        <Card.Img variant="top" src="images/Gift_In_Trend/img4.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                              Roses Combo Harmony for your loves ones
-                            </Card.Text>
-                            <Card.Text>
-                            <b>₹2350</b> 
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
+                    <div className='CuratedGifts_grid_2'>
+                        {Gift_In_Trend_Assets.map((e, index) => (
+                            <div className='CuratedGifts_grid_img_2' key={index}>
+                                <Card style={{ width: '15rem' }}>
+                                    <Card.Img variant="top" src={e.img} />
+                                    <Card.Body>
+                                        <Card.Text>
+                                            {e.desc}
+                                        </Card.Text>
+                                        <Card.Text>
+                                            <b>{e.price}</b>
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        ))}
+                    </div>
 
-
-                <div className='CuratedGifts_grid_img_2'>
-                    <Card style={ {width: '15rem',marginLeft:'2%',marginBottom:'2%'}}>
-                        <Card.Img variant="top" src="images/Gift_In_Trend/img5.webp" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                               Heart full of happiness Hamper
-                            </Card.Text>
-                            <Card.Text>
-                            <b>₹1249</b> 
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
-
+                    <br></br><br></br>
+                </Container>
             </div>
-            <br></br>
-            <br></br>
-        </Container>
-    </div>
-    </>
-  )
+        </>
+    );
 }
 
-export default Gift_In_Trend
+export default Gift_In_Trend;
